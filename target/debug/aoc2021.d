461790f2f6debabfa62ec2551d3990fed7850c03
ozan kececioglu <ozankececioglu@gmail.com>
2020-12-04 22:04:51 +0100

    initial

---
+++ target/debug/aoc2021.d
@@ -0,0 +1,1 @@
+/Users/ozan/Projects/rust/aoc2021/target/debug/aoc2021: /Users/ozan/Projects/rust/aoc2021/src/main.rs
---
+++ target/debug/deps/aoc2021.d
@@ -0,0 +1,5 @@
+/Users/ozan/Projects/rust/aoc2021/target/debug/deps/aoc2021: src/main.rs
+
+/Users/ozan/Projects/rust/aoc2021/target/debug/deps/aoc2021.d: src/main.rs
+
+src/main.rs:
