dbea108a07e7c94315f6328e1f19d1b7062ec28d
ozan kececioglu <ozan.kececioglu@nirx.de>
2020-12-07 19:17:37 +0100

    remove target

--- target/debug/aoc2021.d
+++
@@ -1,1 +0,0 @@
-/Users/ozan/Projects/rust/aoc2021/target/debug/aoc2021: /Users/ozan/Projects/rust/aoc2021/src/main.rs
--- target/debug/deps/aoc2021.d
+++
@@ -1,5 +0,0 @@
-/Users/ozan/Projects/rust/aoc2021/target/debug/deps/aoc2021: src/main.rs
-
-/Users/ozan/Projects/rust/aoc2021/target/debug/deps/aoc2021.d: src/main.rs
-
-src/main.rs:
